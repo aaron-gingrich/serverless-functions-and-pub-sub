43ece4a337142cddbe1820db5772402082202833
aaron-gingrich <78753100+aaron-gingrich@users.noreply.github.com>
2022-04-11 11:52:49 -0500

    Big Bang
    Big Bang

---
+++ README.md
@@ -0,0 +1,25 @@
+# Google Cloud Functions AND Pub/Sub Demonstration
+
+* ## [Demo site here:](https://storage.googleapis.com/quiz8-bucket/index.html)
+
+***What are Google Cloud Functions?***
+
+    Google cloud functions are a quick and easy way to deploy microservices. They are a way of deploying small, light weight, individual api functions. It allows us to forgot about the sercer and just go straight to programming
+
+## Task 1
+
+Create and deploy a Cloud Function using Node.js and GCP CloudFunctions (***serverless***)
+
+HTTP arguments:
+
+* op: “add”, “sub”, ”mul”, ”div”, "pow"
+* data1 (int)
+* data2 (int)
+
+![](img/q7-screenshot.png)
+
+## Task 2
+
+create a pub-sub topic and a subscription to an order topic
+
+![](img/pubsub.png)
---
+++ script.js
@@ -0,0 +1,76 @@
+const data1=document.getElementById("data1");
+const data2=document.getElementById("data2");
+const serverresponse=document.getElementById("response");
+
+function send(opcode){
+    console.log(opcode)
+    let request = new XMLHttpRequest();
+    request.open("GET","https://us-central1-cs4843-q8.cloudfunctions.net/gcf-q8?operation="+opcode+"&data1="+data1.value+"&data2="+data2.value,true);
+    request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    request.send(null);
+    request.onreadystatechange=function(){
+        switch(this.readyState){
+            case 0:
+                serverresponse.innerText="Not initialized connection";
+                break;
+            case 1:
+                serverresponse.innerText="Server connection established";
+                break;
+            case 2:
+                serverresponse.innerText="Server recieved the request";
+                break;
+            case 3:
+                serverresponse.innerText="Server is processing the request";
+                break;
+            case 4:
+                if(request.status!=200){
+                    serverresponse.innerText="Server gave the error "+request.status+": "+this.responseText;
+                }else{
+                    serverresponse.innerText=this.responseText;
+                }
+                break;
+        }
+    }
+}
+function pushpub(){
+    console.log("Pushing to pub");
+    // let request = new XMLHttpRequest();
+    // request.open("GET","https://us-central1-voltaic-inn-345501.cloudfunctions.net/quiz8-ovv180?publish=true",true);
+    // request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    // request.send(null);
+    // request.onreadystatechange=function(){
+    //     switch(this.readyState){
+    //         case 0:
+    //             serverresponse.innerText="Not initialized connection";
+    //             break;
+    //         case 1:
+    //             serverresponse.innerText="Server connection established";
+    //             break;
+    //         case 2:
+    //             serverresponse.innerText="Server recieved the request";
+    //             break;
+    //         case 3:
+    //             serverresponse.innerText="Server is processing the request";
+    //             break;
+    //         case 4:
+    //             if(request.status!=200){
+    //                 serverresponse.innerText="Server gave the error "+request.status+": "+this.responseText;
+    //             }else{
+    //                 serverresponse.innerText=this.responseText;
+    //             }
+    //             break;
+    //     }
+    // }
+}
+function add(){
+    send("add");
+}
+function sub(){
+    send("sub");
+}
+function mul(){
+    send("mul");
+}
+function div(){
+    send("div");
+}
