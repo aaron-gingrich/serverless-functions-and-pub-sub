51ebdaecbd1e6ef8d155164756cdcdd310da01af
aaron-gingrich <78753100+aaron-gingrich@users.noreply.github.com>
2022-04-11 13:33:55 -0500

    Updated styling
    Updated styling

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 const data1=document.getElementById("data1");
 const data2=document.getElementById("data2");
 const serverresponse=document.getElementById("response");
+const get=document.getElementById("get");
+const sp_response=document.getElementById("sp-res");
 
 function send(opcode){
     console.log(opcode)
     let request = new XMLHttpRequest();
-    request.open("GET","https://us-central1-cs4843-q8.cloudfunctions.net/gcf-q8?operation="+opcode+"&data1="+data1.value+"&data2="+data2.value,true);
+    var url = "https://us-central1-cs4843-q8.cloudfunctions.net/gcf-q8?operation="+opcode+"&data1="+data1.value+"&data2="+data2.value
+    get.innerText="GET: " + url;
+    request.open("GET",url,true);
     request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
     request.send(null);
     request.onreadystatechange=function(){
@@ -33,7 +37,8 @@ function send(opcode){
     }
 }
 function pushpub(){
-    console.log("Pushing to pub");
+    sp_response.innerText="Order submited";
+    
     // let request = new XMLHttpRequest();
     // request.open("GET","https://us-central1-voltaic-inn-345501.cloudfunctions.net/quiz8-ovv180?publish=true",true);
     // request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
